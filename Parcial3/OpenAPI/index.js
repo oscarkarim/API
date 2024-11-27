999e0ab5e03a742d123d997c260cb4eb9a347293
oscarkarim <oscar.karim@hotmail.com>
2024-11-27 09:17:17 -0600

    inclusion de readme

--- Parcial3/OpenAPI/index.js
+++ Parcial3/OpenAPI/index.js
@@ -3,17 +3,36 @@ const app = express();
 const swaggerUI = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
 const path = require('path');
-const swaggerOptions = require('./swaggerOptions.json');
-swaggerOptions.apis = [path.join(__dirname, 'index.js')];
-
+const fs = require('fs');
+//const swaggerOptions = require('./swaggerOptions.json');
+//swaggerOptions.apis = [path.join(__dirname, 'index.js')];
+const datosReadme = fs.readFileSync(path.join(__dirname,'readme.md'),{ encoding: 'utf8', flag: 'r' });
 const port = process.env.PORT || 8082;
-
+const swaggerOptions ={
+        "definition": {
+          "openapi": "3.0.0",
+          "info": {
+            "title": "API Empleados",
+            "version": "1.0.0",
+          "description": datosReadme
+          },
+          "servers": [
+            {
+              "url": "http://localhost:{port}"
+            }
+          ]
+        },
+        "apis": [path.join(__dirname, 'index.js')]
+}
+      
 
  
 /**
  * @swagger
  * /empleado:
  *   get:
+ *     tags:
+ *       - empleado
  *     description: Consultar todos los empleados
  *     responses:
  *       200:
@@ -30,6 +49,8 @@ app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(swaggerDocs));
  * @swagger
  * /empleado:
  *   post:
+ *     tags:
+ *       - empleado
  *     description: Alta de Empleados
  *     responses:
  *       200:
@@ -42,3 +63,27 @@ app.listen(port,()=>{
     console.log(`Server Express escuchando en puerto ${port}`)
 })
 
+/**
+* @swagger
+* components:
+*   schemas:
+*       usuario:
+*           type: object
+*           properties:
+*               id_usuario:
+*                   type: number
+*                   example: 10
+*               nombre:
+*                   type: string
+*                   example: Gerardo
+*               ap_paterno:
+*                   type: string
+*                   example: Pineda
+*/
+
+/**
+* @swagger
+* tags:
+* - name: empleado
+*   description: Catalogo de empleados
+*/
